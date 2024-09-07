195d7190d8ca24a675b68ec9f83c76e0e4dc3466
Akhtar Abbas <akhtarbalti1214@gmail.com>
2024-09-07 22:50:45 +0500

    first commit

---
+++ akhtar.dart
@@ -0,0 +1,14 @@
+// void main (){
+//   List name = ['akhtar', 'rizwam', 'sadiq', 'kazim', 'sikandar'];
+//   print(name);
+
+// }
+// void main (){
+//   List weak_name = ['Sunday', 'Monday', 'Tuesday', 'Wednesday','Thursday', 'Friday','Saturday'];
+//   print(weak_name[0]);
+
+// }
+void main(){
+  List student_name = ['akhtar', 'rizwam', 'sadiq', 'kazim', 'sikandar'];  
+
+}
